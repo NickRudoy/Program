2acf8685af6c8cb56d4881d41779e832968bf49f
Nick Rudoy <nickrudoi@yandex.ru>
2022-12-28 11:40:12 +0300

    first commit

---
+++ currency_transfer.py
@@ -0,0 +1,8 @@
+dollas_per_euro = 1.25
+rubles_per_dollars = 60
+
+euro_counts = 100
+dollars_count = euro_counts * dollas_per_euro
+rubles_count = dollars_count * rubles_per_dollars
+
+print (rubles_count)
